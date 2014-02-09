bad1677fa4e272d67be3d48b0d07f7b9431e8c68
Dave Brotherstone <davegb@pobox.com>
2014-02-09 13:33:46 +0100

    Test discovery

--- PythonScript/python_tests/RunTests.py
+++ PythonScript/python_tests/RunTests.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 import sys
 import unittest
+import os
 
 # unittest module expects argv to be set
 sys.argv = ['']
 
-import npp_unit_tests.tests.ReplaceUTF8TestCase
-reload(npp_unit_tests.tests.ReplaceUTF8TestCase)
+(path, runTestsName) = os.path.split(__file__)
 
-import npp_unit_tests.tests.ReplaceAnsiTestCase
-reload(npp_unit_tests.tests.ReplaceAnsiTestCase)
+test_suites = []
 
-import npp_unit_tests.tests.ReplaceUTF8PythonFunction
-reload(npp_unit_tests.tests.ReplaceUTF8PythonFunction)
+for test_name in  os.listdir(os.path.join(path, 'tests')):
+    (test_name, ext) = os.path.splitext(test_name)
+    if ext == '.py':
+        test_module = reload(__import__('npp_unit_tests.tests.' + test_name))
+        test_suite = getattr(test_module.tests, test_name)
+        if hasattr(test_suite, 'suite'):
+            test_suites.append(test_suite.suite)
 
-alltests = unittest.TestSuite([npp_unit_tests.tests.ReplaceUTF8TestCase.suite,
-                               npp_unit_tests.tests.ReplaceAnsiTestCase.suite,
-                               npp_unit_tests.tests.ReplaceUTF8PythonFunction.suite])
+    
+alltests = unittest.TestSuite(test_suites)
+
+console.show()
+console.clear()
 
 
 results = unittest.TestResult()
@@ -26,22 +32,22 @@ alltests.run(results)
 # console.write('Tests Run: {} Errors: {} Failures: {}'.format(results.testRun, results.errors, results.failures))
 
 def writeTestFailure(error):
-	console.write('TEST: %s\n' % error[0])
-	console.writeError(error[1])
-	console.write('\n----------------------------\n')
+    console.write('TEST: %s\n' % error[0])
+    console.writeError(error[1])
+    console.write('\n----------------------------\n')
 
 if results.errors:
-	for error in results.errors:
-		writeTestFailure(error)
-		
+    for error in results.errors:
+        writeTestFailure(error)
+        
 if results.failures:
-	for error in results.failures:
-		writeTestFailure(error)
+    for error in results.failures:
+        writeTestFailure(error)
 
 if results.errors or results.failures:
-	console.writeError('Tests Run: {} Errors: {} Failures: {}\n'.format(results.testsRun, len(results.errors), len(results.failures)))
+    console.writeError('Tests Run: {} Errors: {} Failures: {}\n'.format(results.testsRun, len(results.errors), len(results.failures)))
 else:
-	console.write('Tests Run: {} Errors: {} Failures: {}\n'.format(results.testsRun, len(results.errors), len(results.failures)))
+    console.write('Tests Run: {} Errors: {} Failures: {}\n'.format(results.testsRun, len(results.errors), len(results.failures)))
 
-console.show();
+
 

d6f776c00737c2d8212d4509eb227f2c292119b2
Dave Brotherstone <davegb@pobox.com>
2014-03-22 09:38:02 +0100

    WiX Installer including extra libs + Tcl/Tk
    Tested on clean VM.

---
+++ installer/buildInstaller.bat
@@ -0,0 +1,84 @@
+@echo off
+
+:: Set PYTHONBUILDDIR to the root of your python directory
+:: Or, if you just want to build the installer for PythonScript with an existing python27.dll,
+:: set PYTHONBUILDDIR to a path containing a PCBuild directory, which contains the python27.dll
+
+IF [%PYTHONBUILDDIR%] == [] (
+	SET PYTHONBUILDDIR=l:\code\cpython
+)
+
+SET PYTHONSCRIPTVERSION=1.0.0.0
+
+
+echo Generating WiX information for ..\pythonlib\full
+heat dir ..\pythonlib\full -ag -cg CG_PythonLib -dr D_PythonScript -var var.pylibSource -t changeDirLib.xsl -o temp\fullLib.wxs 
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+echo Compiling python lib WiX source
+candle temp\fullLib.wxs -o temp\fullLib.wixobj -dpylibSource=..\pythonlib\full
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+echo Generating WiX information for ..\pythonlib\extra
+heat dir ..\pythonlib\extra -ag -cg CG_PythonExtraLib -dr D_PythonScript -var var.pylibSource -t changeDirLib.xsl -o temp\extra.wxs
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+echo Compiling extra lib WiX source
+candle temp\extra.wxs -o temp\extra.wixobj -dpylibSource=..\pythonlib\extra
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+echo Generating WiX information for ..\pythonlib\tcl
+heat dir ..\pythonlib\tcl -ag -cg CG_PythonTclTkLib -dr D_PythonScript -var var.pylibSource -t changeDirLib.xsl -o temp\tcl.wxs
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+echo Compiling tcl lib WiX source
+candle temp\tcl.wxs -o temp\tcl.wixobj -dpylibSource=..\pythonlib\tcl
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+
+
+echo Generating WiX information for ..\PythonScript\python_tests
+heat dir ..\PythonScript\python_tests -ag -cg CG_UnitTests -dr D_PythonScript -var var.unittestSource -t changeDirTests.xsl -o temp\unittests.wxs
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+echo Compiling Unit test WiX source
+candle temp\unittests.wxs -o temp\unittests.wixobj -dunittestSource=..\pythonscript\python_tests
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+
+
+echo Compiling main PythonScript installer
+candle pythonscript.wxs -o temp\pythonscript.wixobj -dversion=%PYTHONSCRIPTVERSION% -dbaseDir=.. -dpythonDir=l:\code\cpython
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+
+echo Linking installer - generating MSI
+light temp\pythonscript.wixobj temp\fullLib.wixobj temp\extra.wixobj temp\unittests.wixobj temp\tcl.wixobj -o build\PythonScript_%PYTHONSCRIPTVERSION%.msi -ext WixUIExtension
+if NOT [%ERRORLEVEL%]==[0] (
+ goto error
+)
+
+goto end
+
+:error
+echo Error!
+
+:end

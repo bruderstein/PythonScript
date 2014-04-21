acf330960669b2d26d4f4f38f207c619148cc085
Dave Brotherstone <davegb@pobox.com>
2014-04-21 19:21:35 +0200

    Add test for abs()
    Issue with #define in Python for _hypot

---
+++ PythonScript/python_tests/tests/MathTestCase.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+class MathTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_complex_abs(self):
+        cplx = complex(-2, -1)
+        self.assertAlmostEqual(abs(cplx), 2.236067977, places = 6) 
+
+suite = unittest.TestLoader().loadTestsFromTestCase(MathTestCase)

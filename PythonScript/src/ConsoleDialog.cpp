dd3aaddcc92c9f59086b82e13ad2e4250436a3cf
CFrank <CFrank2512@gmail.com>
2018-08-07 09:51:38 +0200

    slight position adjustment to RUN button

--- PythonScript/src/ConsoleDialog.cpp
+++ PythonScript/src/ConsoleDialog.cpp
@@ -143,7 +143,7 @@ INT_PTR CALLBACK ConsoleDialog::run_dlgProc(UINT message, WPARAM wParam, LPARAM 
             MoveWindow(m_scintilla, 0, 0, LOWORD(lParam), HIWORD(lParam)-30, TRUE);
             MoveWindow(::GetDlgItem(_hSelf, IDC_PROMPT), 0, HIWORD(lParam)-22, 30, 25, TRUE);
             MoveWindow(m_hCombo, 30, HIWORD(lParam)-25, LOWORD(lParam)-85, 25, TRUE);
-            MoveWindow(::GetDlgItem(_hSelf, IDC_RUN), LOWORD(lParam)-50, HIWORD(lParam)-25, 50, 25, TRUE);
+            MoveWindow(::GetDlgItem(_hSelf, IDC_RUN), LOWORD(lParam)-50, HIWORD(lParam)-26, 50, 25, TRUE);
             // ::SendMessage(m_scintilla, WM_SIZE, 0, MAKEWORD(LOWORD(lParam) - 10, HIWORD(lParam) - 30));
             return FALSE;
 

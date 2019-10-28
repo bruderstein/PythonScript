941183bd8b3beaac0fb4bdb7d082df46ffdf9792
Ekopalypse <47723516+Ekopalypse@users.noreply.github.com>
2019-10-28 01:01:23 +0000

    make samples work with scintilla 4.2.0 (#132)

--- scripts/Samples/EnhancedPythonLexer.py
+++ scripts/Samples/EnhancedPythonLexer.py
@@ -111,7 +111,7 @@ except NameError:
 
 
         def on_bufferactivated(self,args):
-            if (self.get_lexer_name() == self.lexer_name) and (editor.getPropertyInt(self.__class__.__name__) != -1):
+            if (self.get_lexer_name() == self.lexer_name) and (editor.getPropertyInt(self.__class__.__name__, 0) != -1):
                 self.__is_lexer_doc = True
             else:
                 self.__is_lexer_doc = False

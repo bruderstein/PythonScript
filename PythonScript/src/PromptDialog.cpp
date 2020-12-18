78bedb519a3bf5cd86af060b304159531b6df846
Christian Grasser <christian.grasser@live.de>
2020-12-19 00:29:37 +0100

    allow unicode texts for the prompt dialog

--- PythonScript/src/PromptDialog.cpp
+++ PythonScript/src/PromptDialog.cpp
@@ -3,6 +3,7 @@
 #include "PromptDialog.h"
 #include "resource.h"
 #include "Notepad_Plus_msgs.h"
+#include "WcharMbcsConverter.h"
 
 PromptDialog::PromptDialog(HINSTANCE hInst, HWND hNotepad)
 	: m_hInst(hInst),
@@ -62,9 +63,9 @@ INT_PTR CALLBACK PromptDialog::runDlgProc(HWND hWnd, UINT message, WPARAM wParam
 		{
 			SendMessage(m_hNotepad, NPPM_MODELESSDIALOG, MODELESSDIALOGADD, reinterpret_cast<LPARAM>(hWnd));
 			m_hSelf = hWnd;
-			::SetWindowTextA(::GetDlgItem(m_hSelf, IDC_PROMPT), m_prompt.c_str());
-			::SetWindowTextA(m_hSelf, m_title.c_str());
-			::SetWindowTextA(::GetDlgItem(m_hSelf, IDC_USERTEXT), m_initial.c_str());
+			::SetWindowText(::GetDlgItem(m_hSelf, IDC_PROMPT), WcharMbcsConverter::char2tchar(m_prompt.c_str()).get());
+			::SetWindowText(m_hSelf, WcharMbcsConverter::char2tchar(m_title.c_str()).get());
+			::SetWindowText(::GetDlgItem(m_hSelf, IDC_USERTEXT), WcharMbcsConverter::char2tchar(m_initial.c_str()).get());
 			::SendMessage(::GetDlgItem(m_hSelf, IDC_USERTEXT), EM_SETSEL, 0, -1);
 			SetFocus(::GetDlgItem(m_hSelf, IDC_USERTEXT));
 
@@ -90,9 +91,9 @@ INT_PTR CALLBACK PromptDialog::runDlgProc(HWND hWnd, UINT message, WPARAM wParam
 			{
 				case IDOK:
 					{
-						char buffer[1000];
-						::GetWindowTextA(::GetDlgItem(m_hSelf, IDC_USERTEXT), buffer, 1000);
-						m_value = buffer;
+						TCHAR buffer[1000];
+						::GetWindowText(::GetDlgItem(m_hSelf, IDC_USERTEXT), buffer, 1000);
+						m_value = WcharMbcsConverter::tchar2char(buffer).get();
 						m_result = RESULT_OK;
 					}
 					//lint -fallthrough

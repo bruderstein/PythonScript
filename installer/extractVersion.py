e139dccce57f66ecdb7a6f921808e6781fd69c01
Christian Grasser <christian.grasser@live.de>
2023-12-21 17:32:03 +0100

    avoid python warning ..\pythonscript\installer\extractVersion.py:7: SyntaxWarning: invalid escape sequence '\P'

--- installer/extractVersion.py
+++ installer/extractVersion.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import sys, os
 import re
 
-pathToVersionFile = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '..\PythonScript\src\PythonScriptVersion.h')
+pathToVersionFile = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '..\\PythonScript\\src\\PythonScriptVersion.h')
 
 versionHeader = open(pathToVersionFile, 'r')
 versionRegex = re.compile(r'PYSCR_VERSION_STRING\s+"([0-9.]+)"')
